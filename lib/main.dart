dbc5bb19af0aa8b17e0537bbb2a9e4c1b9e818bf
arinomami8068 <hebrews12.11.ms@gmail.com>
2021-08-22 22:48:21 +0900

    お腹いっぱい

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter nemuinmusugi',
+      title: 'Flutter nemuinmusugi 12345',
       theme: ThemeData(
         // This is the theme of your application.
         //
